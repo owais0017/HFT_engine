83a8f1976bbd0ebcd1db7244bc03fdc25584e3d4
owais fayaz shah <owaisfayazshah@MacBook-Air.local>
2026-01-21 00:01:36 +0530

    sequencer added

---
+++ build/CMakeFiles/sequencer_app.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/owaisfayazshah/Desktop/Distributed_Order_Matching_Engine/engine/OrderBook.cpp" "CMakeFiles/sequencer_app.dir/engine/OrderBook.cpp.o" "gcc" "CMakeFiles/sequencer_app.dir/engine/OrderBook.cpp.o.d"
+  "/Users/owaisfayazshah/Desktop/Distributed_Order_Matching_Engine/main.cpp" "CMakeFiles/sequencer_app.dir/main.cpp.o" "gcc" "CMakeFiles/sequencer_app.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/sequencer_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sequencer_app.dir/engine/OrderBook.cpp.o"
+  "CMakeFiles/sequencer_app.dir/engine/OrderBook.cpp.o.d"
+  "CMakeFiles/sequencer_app.dir/main.cpp.o"
+  "CMakeFiles/sequencer_app.dir/main.cpp.o.d"
+  "sequencer_app"
+  "sequencer_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sequencer_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
