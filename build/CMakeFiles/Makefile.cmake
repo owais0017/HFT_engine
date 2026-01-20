e157effadba32a385172bc6ec25147520e9933b6
owais fayaz shah <owaisfayazshah@MacBook-Air.local>
2026-01-21 00:06:12 +0530

    gateway added

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -57,4 +57,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/sequencer_app.dir/DependInfo.cmake"
+  "CMakeFiles/gateway_client.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/gateway_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gateway_client.dir/gateway.cpp.o"
+  "CMakeFiles/gateway_client.dir/gateway.cpp.o.d"
+  "gateway_client"
+  "gateway_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gateway_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
